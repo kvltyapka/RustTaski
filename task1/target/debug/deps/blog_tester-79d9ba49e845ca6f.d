02eb4c46ca13425f0ca0f04977015f2cc8b4945b
kvltyapka <kvltyapka@gmail.com>
2024-06-06 15:36:47 +0300

    tests

---
+++ task1/target/debug/deps/blog_tester-79d9ba49e845ca6f.d
@@ -0,0 +1,5 @@
+C:\Users\basss\OneDrive\Рабочий стол\RustRabota\task1\target\debug\deps\blog_tester-79d9ba49e845ca6f.exe: tests\blog-tester.rs
+
+C:\Users\basss\OneDrive\Рабочий стол\RustRabota\task1\target\debug\deps\blog_tester-79d9ba49e845ca6f.d: tests\blog-tester.rs
+
+tests\blog-tester.rs:
---
+++ task1/target/debug/deps/blog_tester-af522f2f3a9864f4.d
@@ -0,0 +1,5 @@
+C:\Users\basss\OneDrive\Рабочий стол\RustRabota\task1\target\debug\deps\libblog_tester-af522f2f3a9864f4.rmeta: tests\blog-tester.rs
+
+C:\Users\basss\OneDrive\Рабочий стол\RustRabota\task1\target\debug\deps\blog_tester-af522f2f3a9864f4.d: tests\blog-tester.rs
+
+tests\blog-tester.rs:
