f736359289667c2b81d670ad39115cbdc020eb55
kvltyapka <kvltyapka@gmail.com>
2024-06-06 08:08:09 +0300

    first

---
+++ task1/target/debug/deps/task1-77cb7292765e9bcc.d
@@ -0,0 +1,5 @@
+C:\Users\basss\OneDrive\Рабочий стол\RustRabota\task1\target\debug\deps\libtask1-77cb7292765e9bcc.rmeta: src\main.rs
+
+C:\Users\basss\OneDrive\Рабочий стол\RustRabota\task1\target\debug\deps\task1-77cb7292765e9bcc.d: src\main.rs
+
+src\main.rs:
